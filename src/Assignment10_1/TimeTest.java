c7148e232410fbd259709e2991d8b7df4e377030
András Ács <5522871+andracs@users.noreply.github.com>
2020-11-06 13:32:20 +0100

    Update TimeTest.java

--- src/Assignment10_1/TimeTest.java
+++ src/Assignment10_1/TimeTest.java
@@ -1,4 +1,4 @@
-package Assignment10_1;
+package Sara;
 
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,29 +7,22 @@ class TimeTest {
 
     @Test
     void getHourTest() {
-        Time time1 = new Time();
-        time1.freezeTime();
-        time1.getHour();
-        System.out.println(time1);
-        assertEquals(13, time1.getHour());
+        Time time1 = new Time(555550000);
+        assertEquals(10, time1.getHour());
     }
 
     @Test
     // OBS! denne kan ikke testes
     void getMinuteTest() {
-        Time time2 = new Time();
-        time2.freezeTime();
-        time2.getMinute();
-        assertEquals(44, time2.getMinute());
+        Time time2 = new Time(555550000);
+        assertEquals(19, time2.getMinute());
     }
 
     @Test
     // OBS! denne kan ikke testes
     void getSecondTest() {
-        Time time3 = new Time();
-//        time3.freezeTime();
-        time3.getSecond();
-        assertEquals(23, time3.getSecond());
+        Time time3 = new Time(555550000);
+        assertEquals(10, time3.getSecond());
     }
 
     @Test
